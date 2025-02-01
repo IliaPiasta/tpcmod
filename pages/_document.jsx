0424de5c26abd73662c8685f90a845ce086e0690
Ilia Piasta <163361903+IliaPiasta@users.noreply.github.com>
2025-01-31 18:38:53 -0800

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,14 +11,14 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Initially, the TpC bot was meant to be integrated with ChatGPT, allowing users to receive AI-driven responses to their queries. However, this idea was later abandoned, shifting the focus toward developing features more tailored to Discord users’ needs. This change allowed TpC to evolve into a versatile tool, capable of automating various tasks, supporting gaming and social interactions, and helping communities engage more effectively."
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta name="twitter:site" content="@tpc" />
+          <meta name="twitter:creator" content="@tpc" />
+          <meta property="og:url" content="https://top.gg/bot/1196199588558819449" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="tpc" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -26,17 +26,17 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Initially, the TpC bot was meant to be integrated with ChatGPT, allowing users to receive AI-driven responses to their queries. However, this idea was later abandoned, shifting the focus toward developing features more tailored to Discord users’ needs. This change allowed TpC to evolve into a versatile tool, capable of automating various tasks, supporting gaming and social interactions, and helping communities engage more effectively."
           />
-          <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="MusicMaker" />
+          <meta property="og:image" content="/img/logo2.jpg" />
+          <meta property="og:image:alt" content="TpC" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="TpC"
           />
           <meta name="theme-color" content="#087ddd" />
-          <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
+          <link rel="icon" href="/img/logo2.jpg" type="image/x-icon" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
             rel="stylesheet"
